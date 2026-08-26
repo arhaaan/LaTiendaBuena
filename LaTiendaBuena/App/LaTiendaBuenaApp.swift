a668e56b84ec7ff96d47fcf00433fe5e0558bdfe
arhaaan <85577285+arhaaan@users.noreply.github.com>
2026-08-26 21:14:08 +0700

    Add files via upload

---
+++ LaTiendaBuena/App/LaTiendaBuenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaTiendaBuenaApp.swift
+//  LaTiendaBuena
+//
+//  Created by Arhan on 25/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LaTiendaBuenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaTiendaBuena/Core/Features/Authentications/Views/LoginView.swift
@@ -0,0 +1,77 @@
+//
+//  LoginView.swift
+//  LaTiendaBuena
+//
+//  Created by Arhan on 25/08/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var isLoading = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image(.supabase)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                Button {} label: {
+                    Text("Login")
+                        .font(.headline)
+                        .frame(width: 360, height: 48)
+                        .background(.blue)
+                        .cornerRadius(8)
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    RegistrationView()
+                        .navigationBarBackButtonHidden()
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.subheadline)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
